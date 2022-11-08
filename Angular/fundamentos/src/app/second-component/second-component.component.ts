86caf6880df0c6f652879f328c73bb8c3e04ba3c
MathCoimbra <matheuscoimbra35@gmail.com>
2022-11-08 16:42:56 -0300

    add event binding example
    Signed-off-by: MathCoimbra <matheuscoimbra35@gmail.com>

--- Angular/fundamentos/src/app/second-component/second-component.component.ts
+++ Angular/fundamentos/src/app/second-component/second-component.component.ts
@@ -5,7 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './second-component.component.html',
   styleUrls: ['./second-component.component.css']
 })
-export class SecondComponentComponent{
+export class SecondComponentComponent {
   name = "Matheus";
-  birthDate = "2001/09/29";
+  birthDate = new Date("2001/09/29");
+  imageURL = "/assets/Foto.jpg";
+  today = new Date();
+  years = this.birthDate.getDate() - this.today.getDate();
+
+  showAge() {
+    alert(`He is ${this.years} years old`);
+  }
 }
