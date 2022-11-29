805a320f19c30a933c79815ab8e814f8b2f2b7c6
MathCoimbra <matheuscoimbra35@gmail.com>
2022-11-29 15:04:47 -0300

    feat: add redirection example
    Signed-off-by: MathCoimbra <matheuscoimbra35@gmail.com>

--- Angular/roteamento/src/app/app-routing.module.ts
+++ Angular/roteamento/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstPageComponent } from './first-page/first-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SecondPageComponent } from './second-page/second-page.component';
 
 
 const routes: Routes = [
   { path: "first-page", component: FirstPageComponent },
-  { path: "second-page", component: SecondPageComponent}
-]
+  { path: "second-page", component: SecondPageComponent },
+  /* redirecionamento quando a página não tiver nenhum endereço o full é para garantir que somente será redirecionado quando o path estiver vazio */
+  { path: "", redirectTo: "first-page", pathMatch: "full" },
+  /* dois asteriscos serve para quando há uma URL desconhecida, não configurada no sistema, */
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [],
--- Angular/roteamento/src/app/app.module.ts
+++ Angular/roteamento/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { FirstPageComponent } from './first-page/first-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SecondPageComponent } from './second-page/second-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstPageComponent,
-    SecondPageComponent
+    SecondPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
