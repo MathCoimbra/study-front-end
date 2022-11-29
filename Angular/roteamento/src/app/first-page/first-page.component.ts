21492b6374801fe513b4a5d1fc82eff18812b027
MathCoimbra <matheuscoimbra35@gmail.com>
2022-11-29 14:34:40 -0300

    feat: add navigation between pages with Router
    Signed-off-by: MathCoimbra <matheuscoimbra35@gmail.com>

--- Angular/roteamento/src/app/first-page/first-page.component.ts
+++ Angular/roteamento/src/app/first-page/first-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-first-page',
   templateUrl: './first-page.component.html',
   styleUrls: ['./first-page.component.css']
 })
-export class FirstPageComponent implements OnInit {
+export class FirstPageComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  goToSecondPage(){
+    this.router.navigate(["second-page"])
   }
 
 }
