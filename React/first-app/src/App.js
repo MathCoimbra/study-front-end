4a97b69497bdc099fef3ca3717ffa038cd1cfe58
MathCoimbra <matheuscoimbra35@gmail.com>
2022-12-15 16:49:22 -0300

    feat: create button component
    Signed-off-by: MathCoimbra <matheuscoimbra35@gmail.com>

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -1,5 +1,12 @@
+import Button from './components/button';
+
 function App() {
-  return <div className="App">Hello React!</div>;
+  return (<div className="App">
+    <h1>Hello React!</h1>
+    <Button title ="Join" />
+    <Button title ="Leave" />
+    </div>
+    );
 }
 
 export default App;
